41e9b18c56bcccf8881474e53ee114044ae68ca2
l3oss123456 <oncelemon02@gmail.com>
2021-03-20 14:43:02 +0700

    update

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const express = require("express");
 const { Server } = require("ws");
 
 const PORT = process.env.PORT || 9000;
-const INDEX = "/index.html";
+// const INDEX = "/index.html";
 const { list_covid19_all_country } = require("./src/domains/index");
 const { broadcastData } = require("./src/utils/domains");
 
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use((req, res) => res.sendFile({ root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
